23e4c4fd80dc92cce4b0bcd569fdf89d23beecf7
Anvar <fort2652@gmail.com>
2023-01-23 15:29:22 +0300

    fix: tests not work properly

---
+++ src/check.js
@@ -0,0 +1,94 @@
+const MORSE_TABLE = {
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
+};
+
+let code = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
+let dot = /10/g
+let dash = /11/g
+let space =  /[*]{10}/g
+
+let toMorse = code
+  .replace(dash, '-')
+  .replace(dot, '.')
+  // .replace(space, ' ')
+  // .replace(/0/g,' ')
+
+let str = []
+console.log('toMorse',toMorse)
+let arrs = toMorse.split(' ')
+// .map(i => i.split('**********'))
+.flat().map(i => i.split('0'))
+.flat().map(i => i.length > 5 ? i.split('**********') : i)
+
+// .map(i => i.split('0'))
+// let morseLetters = arrs.flat().map(i => i.split('0'))
+let morseLetters = []
+//= arrs.filter(i => i !== '').map(i => i.length > 1)
+for (let i = 0;i < arrs.length;i++){
+  if(arrs[i] !== ''){
+    console.log('arrs[i]',arrs[i])
+    console.log('MORSE_TABLE[arrs[i]]',MORSE_TABLE[arrs[i]])
+    if(Array.isArray(arrs[i])){
+      morseLetters.push(MORSE_TABLE[arrs[i][0]]) 
+      morseLetters.push('space')
+    }else{
+      morseLetters.push(MORSE_TABLE[arrs[i]]) 
+    }
+  }
+}
+console.log('arrs',arrs)
+console.log('arrs[0]',arrs[0])
+console.log('morseLetters',morseLetters.join('').replace('space',' '))
+//   toMorse.split('0').forEach(letter => {
+//     console.log("letter",letter)
+//     if(MORSE_TABLE[letter] !== undefined)
+//       str.push(MORSE_TABLE[letter])
+//   })
+
+// console.log("arrs.join(' ')",arrs.join(' '))
+
+// arrs.forEach(arr => {
+//   arr.forEach(letter => {
+//         if(MORSE_TABLE[letter] !== undefined)
+//       str.push(MORSE_TABLE[letter])
+//   })
+// })
+// console.log("toMorse",toMorse)
+// console.log("toMorse.split('')",toMorse.split('0'))
+// console.log("str",str)
+// console.log("MORSE_TABLE['....']",MORSE_TABLE['....'])
--- src/index.js
+++ src/index.js
@@ -38,7 +38,56 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  // let dot = /10/g
+  // let dash = /11/g
+  // let space =  /[*]{10}/g
+
+  // expr
+  // // .replace(dot,'.')
+  // .replace(dash, '-')
+  // // .replace(space, ' ')
+  // console.log('expr',expr)
+  // return expr
+
+  let code = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
+let dot = /10/g
+let dash = /11/g
+let space =  /[*]{10}/g
+
+let toMorse = code
+  .replace(dash, '-')
+  .replace(dot, '.')
+  // .replace(space, ' ')
+  // .replace(/0/g,' ')
+
+let str = []
+console.log('toMorse',toMorse)
+let arrs = toMorse.split(' ')
+// .map(i => i.split('**********'))
+.flat().map(i => i.split('0'))
+.flat().map(i => i.length > 5 ? i.split('**********') : i)
+
+// .map(i => i.split('0'))
+// let morseLetters = arrs.flat().map(i => i.split('0'))
+let morseLetters = []
+//= arrs.filter(i => i !== '').map(i => i.length > 1)
+for (let i = 0;i < arrs.length;i++){
+  if(arrs[i] !== ''){
+    console.log('arrs[i]',arrs[i])
+    console.log('MORSE_TABLE[arrs[i]]',MORSE_TABLE[arrs[i]])
+    if(Array.isArray(arrs[i])){
+      morseLetters.push(MORSE_TABLE[arrs[i][0]]) 
+      morseLetters.push('space')
+    }else{
+      morseLetters.push(MORSE_TABLE[arrs[i]]) 
+    }
+  }
+}
+// console.log('arrs',arrs)
+// console.log('arrs[0]',arrs[0])
+// console.log('morseLetters',morseLetters.join('').replace('space',' '))
+
+return morseLetters.join('').replace('space',' ')
 }
 
 module.exports = {
